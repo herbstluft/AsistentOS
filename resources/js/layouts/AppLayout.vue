c532b5a4ef285df26f69294f2a60de0d406e9715
castaneda <castaneda@MacBook-Air-de-castaneda.local>
2025-12-28 04:58:04 -0600

    remove  banner

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -2,7 +2,7 @@
 import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
 import ConfirmDialog from '@/components/ConfirmDialog.vue';
 import ToastNotifications from '@/components/ToastNotifications.vue';
-import TrialBanner from '@/components/TrialBanner.vue';
+
 import SubscriptionBlockedScreen from '@/components/SubscriptionBlockedScreen.vue';
 import type { BreadcrumbItemType } from '@/types';
 import { useAppointmentReminders } from '@/composables/useAppointmentReminders';
@@ -135,7 +135,6 @@ const trialUsed = computed(() => {
 
     <!-- Contenido Normal -->
     <template v-else>
-        <TrialBanner />
         <AppLayout :breadcrumbs="breadcrumbs">
             <slot />
         </AppLayout>
